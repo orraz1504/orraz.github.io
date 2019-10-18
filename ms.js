ec5a1e0bfa51a647c35dbe504f293fc0dff12e1e
orraz1504 <46423524+orraz1504@users.noreply.github.com>
2019-10-18 23:49:21 +0300

    Gameweek

--- ms.js
+++ ms.js
@@ -1,11 +1,4 @@
-//$.getJSON(
-//    'https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=3dda74e1a011bc4dbd71df4f1588c45fca7291daa563ef963877c3b6445e5c43',
-//    function(data){
-//        console.log(data);
-//    }
-//);
 window.addEventListener("load", ()=>{
-    let place;
     const api ='https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=3dda74e1a011bc4dbd71df4f1588c45fca7291daa563ef963877c3b6445e5c43';
 
     fetch(api)
@@ -14,14 +7,15 @@ window.addEventListener("load", ()=>{
         })
         .then(data=>{
             console.log(data)
-            console.log(data[0].overall_league_position)
             var i;
             var j;
             var x = document.getElementsByClassName("howfar");
             var z = document.getElementsByClassName("name");
             var p= document.getElementsByClassName("pos");
-
-            console.log(x[0].textContent+ data[0].team_name)
+            var g=document.getElementsByClassName("gameweek")
+            var t=0;
+     
+            console.log(data)
             for (i = 0; i < data.length; i++) {
                 for (j = 0; j < x.length; j++) {
                     if(data[i].team_name==z[j].textContent){
@@ -38,7 +32,11 @@ window.addEventListener("load", ()=>{
                             x[j].style.background="yellow";
                         }
                     }
+                    if(parseInt(data[i].overall_league_payed)>t){
+                        t=parseInt(data[i].overall_league_payed);
+                    }
                 }
             }
+            g[0].textContent="Game Week " + t;
         })
 });
