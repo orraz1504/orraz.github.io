4cd42ad9eb8373aaac65888202f50718debd411b
orraz1504 <46423524+orraz1504@users.noreply.github.com>
2020-02-29 15:08:43 +0200

    new api

--- ms.js
+++ ms.js
@@ -1,42 +1,42 @@
 window.addEventListener("load", ()=>{
-    const api ='https://apiv2.apifootball.com/?action=get_standings&league_id=148&APIkey=9c72c49bc566a1cba00f796890b765764e93d3a35aece5ecd76a411b16bcc963';
-
-    fetch(api)
-        .then(res =>{
-            return res.json();
-        })
-        .then(data=>{
-            console.log(data)
-            var i;
-            var j;
-            var x = document.getElementsByClassName("howfar");
-            var z = document.getElementsByClassName("name");
-            var p= document.getElementsByClassName("pos");
-            var g=document.getElementsByClassName("gameweek")
-            var t=0;
-     
-            console.log(data)
-            for (i = 0; i < data.length; i++) {
-                for (j = 0; j < x.length; j++) {
-                    if(data[i].team_name==z[j].textContent){
-                        if(parseInt(p[j].textContent)>parseInt(data[i].overall_league_position)){
-                            x[j].textContent= "+"+(Math.abs(parseInt(p[j].textContent-parseInt(data[i].overall_league_position))).toString());
-                            x[j].style.background="green";
-                        }
-                        else if(parseInt(p[j].textContent)<parseInt(data[i].overall_league_position)){
-                            x[j].textContent= "-"+(Math.abs(parseInt(data[i].overall_league_position)-parseInt(p[j].textContent)).toString());
-                            x[j].style.background="red";
-                        }
-                        else{
-                            x[j].textContent= "0";
-                            x[j].style.background="yellow";
-                        }
+    $.ajax({
+        headers: { 'X-Auth-Token': '728c349faecd47eab36bbb6b8a952bbb' },
+        url: 'https://api.football-data.org/v2/competitions/PL/standings',
+        dataType: 'json',
+        type: 'GET',
+      }).done(function(response) {
+        console.log(response);
+        let x = document.getElementsByClassName("howfar");
+        let z = document.getElementsByClassName("name");
+        let p = document.getElementsByClassName("pos");
+        let g = document.getElementsByClassName("gameweek")
+        g[0].textContent="Game Week " + response.season.currentMatchday;
+        for(let i = 0; i < response.standings[0].table.length; i++){
+            for(let j = 0; j < x.length; j++){
+                let tmn = response.standings[0].table[i].team.name.substr(0, response.standings[0].table[i].team.name.length-3);
+                if(tmn == "Wolverhampton Wanderers"){
+                    tmn= "Wolves";
+                }else if(tmn == "AFC Bournemo"){
+                    tmn = "Bournemouth";
+                }else if(tmn == "Brighton & Hove Albion"){
+                    tmn = "Brighton";
+                }
+                if(tmn == z[j].textContent){
+                    let pos = response.standings[0].table[i].position;
+                    if(parseInt(p[j].textContent)>parseInt(pos)){
+                        x[j].textContent= "+"+(Math.abs(parseInt(p[j].textContent-parseInt(pos))).toString());
+                        x[j].style.background="green";
                     }
-                    if(parseInt(data[i].overall_league_payed)>t){
-                        t=parseInt(data[i].overall_league_payed);
+                    else if(parseInt(p[j].textContent)<parseInt(pos)){
+                        x[j].textContent= "-"+(Math.abs(parseInt(pos)-parseInt(p[j].textContent)).toString());
+                        x[j].style.background="red";
+                    }
+                    else{
+                        x[j].textContent= "0";
+                        x[j].style.background="yellow";
                     }
                 }
             }
-            g[0].textContent="Game Week " + t;
-        })
+        }
+      });
 });
